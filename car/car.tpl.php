2e21f3e86befd1bb45a1fc024336d30627cb62e2
Shoaib Nawaz <shoaib.nawaz@280200.no-reply.drupal.org>
2014-04-18 00:37:43 +0500

    View mode argument is fixed to be passed to template file for contextual rendering.

--- car/car.tpl.php
+++ car/car.tpl.php
@@ -1,41 +1,22 @@
-<?php
- //print_r($element);
-?>
-
-<div class="row">
-	<span>Car make:</span>
-	<span><?php print $element->make; ?></span>
-</div>
-<div class="row">
-	<span>Car model:</span>
-	<span><?php print $element->model; ?></span>
-</div>
-<div class="row">
-	<span>Car color:</span>
-	<span><?php print $element->color; ?></span>
-</div>
-<div class="row">
-	<span>Top speed:</span>
-	<span><?php print $element->top_speed; ?> MPH</span>
-</div>
-<div class="row">
-	<span>Price:</span>
-	<span>$<?php print $element->price; ?></span>
-</div>
-
-<style>
-	.row > :first-child {
-		width: 30%;
-		float: left;
-		display: inline-block;
-		font-size: 1.2rem;
-		clear: left;
-	}
-
-	.row > :last-child {
-		width: 69%;
-		float: left;
-		display: inline-block;
-		font-size: 1.2rem;
-	}
-</style>
+<?php if($view_mode == 'full'): ?>
+	<div class="row">
+		<span>Car make:</span>
+		<span><?php print $element->make; ?></span>
+	</div>
+	<div class="row">
+		<span>Car model:</span>
+		<span><?php print $element->model; ?></span>
+	</div>
+	<div class="row">
+		<span>Car color:</span>
+		<span><?php print $element->color; ?></span>
+	</div>
+	<div class="row">
+		<span>Top speed:</span>
+		<span><?php print $element->top_speed; ?> MPH</span>
+	</div>
+	<div class="row">
+		<span>Price:</span>
+		<span>$<?php print $element->price; ?></span>
+	</div>
+<?php endif; ?>
